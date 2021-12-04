2e4ff6f8c33c46d1084fb885af47f651b02e0e49
sanjeebsomu <83944578+sanjeebsomu@users.noreply.github.com>
2021-12-04 09:37:46 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+let screen = document.getElementById('screen');
+let buttons = document.querySelectorAll('button');
+let screenValue = '';                   //declaring screenvalue variable
+for(items of buttons){
+    items.addEventListener('click', (e)=>{
+        buttonText = e.target.innerText;
+        if (buttonText == '×') {
+            buttonText = '*';
+            screen.value += buttonText;//herer screen is a id, and we put button text value inside screen
+        }
+        else if(buttonText == 'C'){
+            screen.value = '';          //empty the screen value
+        }
+        else if(buttonText == '='){
+            screen.value = '';          //first empty the screen value then evaluate
+            screen.value += eval(screenValue)
+        }
+        else{
+            screen.value += buttonText;//assinging button text value to  screen.value
+            screenValue = screen.value;//assigning screenValue to screen.value
+        }
+    })
+}
